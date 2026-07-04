948ee281e31b86bffaa705ce8c01e9ab8c8780dd
not specified <johnjojojojojo@proton.me>
2026-07-04 11:34:27 -0400

    Add files via upload

---
+++ howmanyleft.go
@@ -0,0 +1,40 @@
+package main
+import (
+	"fmt"
+	"os"
+	"strconv"
+	)
+
+func main() {
+	if(len(os.Args) < 4){
+		fmt.Println("usage: howmanyleft (%start <> %goal) %inc")
+		os.Exit(0)
+	}
+	start,err := strconv.Atoi(os.Args[1])
+	if( err != nil) {
+		fmt.Println("error reading arg 1:",err)
+		os.Exit(-1)
+	}
+	end,err := strconv.Atoi(os.Args[2])
+	if( err != nil) {
+		fmt.Println("error reading arg 2:",err)
+		os.Exit(-1)
+	}
+	inc,err := strconv.Atoi(os.Args[3])
+	if( err != nil) {
+		fmt.Println("error reading arg 3:",err)
+		os.Exit(-1)
+	}
+	if end == start {
+		fmt.Println(end,"to",start," == 0")
+	}
+	if end < start {
+		end,start = start,end
+	}
+	var j int
+	for i := start; i<end; i+=inc {
+		j++
+	}
+	fmt.Println(start,"to",end,"by",inc,"==",j);
+
+}
