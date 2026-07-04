c857933e68ca086b44e35d44c988670ea159425b
not specified <johnjojojojojo@proton.me>
2026-07-04 11:45:37 -0400

    Add files via upload

--- howmanyleft.go
+++ howmanyleft.go
@@ -1,40 +1,36 @@
 package main
+
 import (
 	"fmt"
 	"os"
 	"strconv"
-	)
+)
 
 func main() {
-	if(len(os.Args) < 4){
+	if len(os.Args) < 4 {
 		fmt.Println("usage: howmanyleft (%start <> %goal) %inc")
 		os.Exit(0)
 	}
-	start,err := strconv.Atoi(os.Args[1])
-	if( err != nil) {
-		fmt.Println("error reading arg 1:",err)
-		os.Exit(-1)
+	var numbers [3]int
+	var err error
+	for i := range numbers {
+		numbers[i] ,err = strconv.Atoi(os.Args[i+1])
+		if err != nil {
+			fmt.Println("err reading arg",i,":",err)
+			os.Exit(-1);
+		}
 	}
-	end,err := strconv.Atoi(os.Args[2])
-	if( err != nil) {
-		fmt.Println("error reading arg 2:",err)
-		os.Exit(-1)
-	}
-	inc,err := strconv.Atoi(os.Args[3])
-	if( err != nil) {
-		fmt.Println("error reading arg 3:",err)
-		os.Exit(-1)
-	}
+	start,end,inc := numbers[0],numbers[1],numbers[2]
 	if end == start {
-		fmt.Println(end,"to",start," == 0")
+		fmt.Println(end, "to", start, " == 0")
 	}
 	if end < start {
-		end,start = start,end
+		end, start = start, end
 	}
 	var j int
-	for i := start; i<end; i+=inc {
+	for i := start; i < end; i += inc {
 		j++
 	}
-	fmt.Println(start,"to",end,"by",inc,"==",j);
+	fmt.Println(start, "to", end, "by", inc, "==", j)
 
 }
